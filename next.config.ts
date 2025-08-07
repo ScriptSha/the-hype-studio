a0058fb0fa174706e086a26908544c3c68c8d24b
ScriptSha <pratikshasable95@gmail.com>
2025-08-07 12:03:26 +0530

    Update configuration and dependencies for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: '/the-hype-studio/', // Replace with your GitHub repo name
+  basePath: '/the-hype-studio',     // Same here
   images: {
     unoptimized: true,
   },
